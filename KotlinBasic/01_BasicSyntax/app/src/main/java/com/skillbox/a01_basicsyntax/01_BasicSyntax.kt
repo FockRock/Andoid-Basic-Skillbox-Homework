768135cdac7bc0983150621c73c90b99d622e536
FockRock <smirnov_nikita@mail.ru>
2020-08-15 00:56:52 +0300

    Lesson 1 (Basic Syntax)

--- KotlinBasic/01_BasicSyntax/app/src/main/java/com/skillbox/a01_basicsyntax/01_BasicSyntax.kt
+++ KotlinBasic/01_BasicSyntax/app/src/main/java/com/skillbox/a01_basicsyntax/01_BasicSyntax.kt
@@ -2,4 +2,31 @@ package com.skillbox.a01_basicsyntax
 
 fun main() {
 
+    val firstName = "Nick"
+    val secondName = "Smirnov"
+    var height = 181
+    val weight = 92.2F
+    val isChild: Boolean = height < 150 && weight < 40
+
+    var info = """
+        My name is $firstName
+        My second name is $secondName
+        My height - $height sm
+        My weight is $weight kg
+        I am ${if(isChild) "" else "not"} child.
+    """
+    println(info)
+
+    height += 10
+    info = """
+        Information Upgraded!        
+        
+        My name is $firstName
+        My second name is $secondName
+        My NEW height - $height sm
+        My weight is $weight kg
+        I am ${if(isChild) "" else "not"} child.
+    """
+
+    println(info)
 }

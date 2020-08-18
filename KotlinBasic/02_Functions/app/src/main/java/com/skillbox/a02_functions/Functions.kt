4bef472a3eaed0bde6ae88abaa2b072458f1e16d
FockRock <smirnov_nikita@mail.ru>
2020-08-19 00:02:33 +0300

    Lesson 2 (Functions) - Upgraded

--- KotlinBasic/02_Functions/app/src/main/java/com/skillbox/a02_functions/Functions.kt
+++ KotlinBasic/02_Functions/app/src/main/java/com/skillbox/a02_functions/Functions.kt
@@ -2,7 +2,7 @@ package com.skillbox.a02_functions
 import kotlin.math.sqrt
 
 fun main() {
-    val solutionSum = solveEquation(a=8, b=0, c=0)
+    val solutionSum = solveEquation(a=8, b=67, c=12)
     println("Сумма корней ровна $solutionSum")
 }
 
@@ -10,22 +10,28 @@ fun solveEquation (a: Int, b: Int, c: Int): Float {
 
     println("$a x^2 ${if(b>=0)"+ $b" else "$b"} x ${if(c>=0)"+ $c" else "$c"} = 0")
 
-    val d: Float = (b*b) - (4F*(a*c))       //формула расчета дискриминанта
+    val d: Float = (b*b) - (4f*(a*c))                  //формула расчета дискриминанта
         println("Дискриминант равен $d")
-    val x1: Float
-    val x2: Float
+    val x1: Any
+    val x2: Any
 
-    if (d > 0) {
-        x1 = ((-1 * b) + sqrt(d)) / 2 * a   //расчет первого корня
-        println("X1 = $x1")
+    when {
+        d > 0 -> {
+            x1 = ((-1 * b) + sqrt(d)) / (2 * a)     //расчет первого корня
+            println("X1 = $x1")
 
-        x2 = ((-1 * b) - sqrt(d)) / 2 * a   //расчет второго корня
-        println("X2 = $x2")
-    } else {
-        x1 = 0F
-        println("X1 = $x1")
-        x2 = 0F
-        println("X1 = $x1")
+            x2 = ((-1 * b) - sqrt(d)) / (2 * a)     //расчет второго корня
+            println("X2 = $x2")
+        }
+        d < 0 -> {
+            x1 = 0f
+            x2 = 0f
+        }
+        else -> {
+            x1 = (-1f * b) / (2f * a)
+            x2 = 0f
+        }
     }
-    return x1 + x2                          //расчет суммы корней
+
+    return x1 + x2                                   //расчет суммы корней
 }
---
+++ KotlinBasic/03_Loops_Recursion_NullableTypes/app/src/main/java/com/skillbox/a03_loops_recursion_nullabletypes/03_Loops_Recursion_NullableTypes.kt
@@ -0,0 +1,5 @@
+package com.skillbox.a03_loops_recursion_nullabletypes
+
+fun main() {
+
+}

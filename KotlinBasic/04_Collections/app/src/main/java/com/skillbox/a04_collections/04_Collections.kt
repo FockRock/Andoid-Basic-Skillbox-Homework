ad5aad54f9b0670863f797cb628a3b7195c4f66b
FockRock <smirnov_nikita@mail.ru>
2020-09-20 13:26:39 +0300

    Homework 4 finished!

--- KotlinBasic/04_Collections/app/src/main/java/com/skillbox/a04_collections/04_Collections.kt
+++ KotlinBasic/04_Collections/app/src/main/java/com/skillbox/a04_collections/04_Collections.kt
@@ -1,5 +1,47 @@
 package com.skillbox.a04_collections
 
-fun main() {
+import android.renderscript.Sampler
+import java.security.Key
 
+fun main() {
+    print("Введите число: ")
+    val n: Int = readLine()?.toIntOrNull()?: return
+
+    if (n > 0) {
+        println("Введите $n номеров телефонов:")
+    } else println("Вы ввели неверное число.")
+
+    val phoneList = phoneListAdd(n)
+    val truePhoneList = phoneList.filter { it.startsWith("+7") }
+    println("Из них настоящие номера, начинающиеся на +7: $truePhoneList")
+
+    val phoneSet = truePhoneList.toMutableSet()
+    val x = phoneSet.size
+    println("Количество уникальных телефонов: $x")
+
+    println("Длина всех номеров телефонов: ${truePhoneList.sumBy { it.length }}")
+
+    var phoneMap = mutableMapOf<String, String>()
+
+    for (newNumber in phoneSet) {
+        println("Введите имя человека с номером телефона $newNumber: ")
+        val phoneName: String = readLine().toString()
+        phoneMap[newNumber] = phoneName
+    }
+
+    for (keys in phoneMap.keys) {
+        println("Человек: ${phoneMap[keys]}. Номер телефона: $keys")
+    }
 }
+
+fun phoneListAdd (phoneNumbers: Int): List<String> {
+    val phoneList = mutableListOf<String>()
+    var num = phoneNumbers
+
+    while (num > 0) {
+        val currentNumber = readLine().toString()
+        phoneList.add(currentNumber)
+        num--
+    }
+    return phoneList
+    }

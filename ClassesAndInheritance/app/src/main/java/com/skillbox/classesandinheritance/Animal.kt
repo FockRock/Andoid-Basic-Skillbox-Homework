58fa62589f67745108434eca843cebeb9ce649e0
FockRock <smirnov_nikita@mail.ru>
2020-11-04 21:50:05 +0300

    Homework 5 started (constructor added)!

---
+++ ClassesAndInheritance/app/src/main/java/com/skillbox/classesandinheritance/Animal.kt
@@ -0,0 +1,11 @@
+package com.skillbox.classesandinheritance
+
+class Animal constructor(
+        var energy: Int,
+        var weight: Int,
+        var age: Int = 0,
+        val maxAge: Int,
+        val name: String
+){
+
+}

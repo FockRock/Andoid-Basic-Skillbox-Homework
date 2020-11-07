0295a7b27a42590837f2df0f03bf11f585e47b27
FockRock <smirnov_nikita@mail.ru>
2020-11-07 16:41:09 +0300

    Homework 5 started (2 completed)

--- ClassesAndInheritance/app/src/main/java/com/skillbox/classesandinheritance/Animal.kt
+++ ClassesAndInheritance/app/src/main/java/com/skillbox/classesandinheritance/Animal.kt
@@ -3,9 +3,9 @@ package com.skillbox.classesandinheritance
 class Animal constructor(
         var energy: Int,
         var weight: Int,
-        var age: Int = 0,
         val maxAge: Int,
         val name: String
 ){
+    var age: Int = 0
     val int: Int = 1
 }

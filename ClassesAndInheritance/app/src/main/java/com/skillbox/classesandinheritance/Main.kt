066fd52c50a203f9917afe97645ae5380062137e
FockRock <smirnov_nikita@mail.ru>
2020-11-13 21:29:48 +0300

    Homework 5 completed!

--- ClassesAndInheritance/app/src/main/java/com/skillbox/classesandinheritance/Main.kt
+++ ClassesAndInheritance/app/src/main/java/com/skillbox/classesandinheritance/Main.kt
@@ -10,18 +10,30 @@ fun main() {
 
     while (n > 0) {
         if (zoo.animalList.size > 0) {
+            val newList = mutableListOf<Animal>()
+            val deadList = mutableListOf<Animal>()
             zoo.animalList.forEach() {
+
+                if (it is Soundable) {
+                    val sound = Random.nextBoolean()        //Может издать звук
+                    if (sound) it.makeSound()
+                }
+
                 if (!it.isTooOld) {
                     when (Random.nextInt(until = 4)) {
                         0 -> it.eat()
                         1 -> it.move()
                         2 -> it.sleep()
-                        3 -> zoo.animalList.add(it.makeChild())
+                        3 -> newList.add(it.makeChild())            //Добавляет ребенка в новый список
                     }
-                } else zoo.animalList.remove(it)
+                }
+                else deadList.add(it)                               //Добавляет в список мертвых
             }
+            zoo.animalList.removeAll(deadList)
+            zoo.animalList.addAll(newList)
             n--
-            println("Осталось живых животных в зоопарке ${zoo.animalList.size}")
+            println("Осталось живых животных в зоопарке: ${zoo.animalList.size}," +
+                    "родилось за все время: ${newList.size}, умерло за все время: ${deadList.size}")
         } else println("В зоопарке пусто!")
     }
 

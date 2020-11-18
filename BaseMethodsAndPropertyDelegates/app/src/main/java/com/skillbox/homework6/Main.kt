8b755065b39de931c84248b6c7c7ebfdb5137ea5
FockRock <smirnov_nikita@mail.ru>
2020-11-18 23:24:47 +0300

    Homework 6 (7/13 completed)

--- BaseMethodsAndPropertyDelegates/app/src/main/java/com/skillbox/homework6/Main.kt
+++ BaseMethodsAndPropertyDelegates/app/src/main/java/com/skillbox/homework6/Main.kt
@@ -3,4 +3,12 @@ package com.skillbox.homework6
 fun main() {
     val person1 = Person(180, 80, "Nick")
     val person2 = Person(180, 80, "Nick")
+    val person3 = Person(170, 70, "Max")
+
+    val hs = hashSetOf<Person>(
+            person1,
+            person2,
+            person3
+    )
+    println(hs)
 }
--- BaseMethodsAndPropertyDelegates/app/src/main/java/com/skillbox/homework6/Person.kt
+++ BaseMethodsAndPropertyDelegates/app/src/main/java/com/skillbox/homework6/Person.kt
@@ -1,9 +1,27 @@
 package com.skillbox.homework6
 
 class Person (
-    height: Int,
-    weight: Int,
-    name: String
+    var height: Int,
+    var weight: Int,
+    var name: String
 ) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
+        other as Person
+
+        if (height != other.height) return false
+        if (weight != other.weight) return false
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = height
+        result = 31 * result + weight
+        result = 31 * result + name.hashCode()
+        return result
+    }
 }

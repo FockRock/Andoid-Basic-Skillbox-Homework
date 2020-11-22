0722bca5f3cbadbbe947c87af368035441963ac6
FockRock <smirnov_nikita@mail.ru>
2020-11-22 22:31:35 +0300

    Homework 6 (11/13 completed)

--- BaseMethodsAndPropertyDelegates/app/src/main/java/com/skillbox/homework6/Main.kt
+++ BaseMethodsAndPropertyDelegates/app/src/main/java/com/skillbox/homework6/Main.kt
@@ -5,10 +5,17 @@ fun main() {
     val person2 = Person(180, 80, "Nick")
     val person3 = Person(170, 70, "Max")
 
-    val hs = hashSetOf<Person>(
+    val hs = hashSetOf(
             person1,
             person2,
             person3
     )
-    println(hs)
+
+    person1.buyPet()
+    person1.buyPet()
+    person1.buyPet()
+
+    hs.forEach() {
+        println(it.toString())
+    }
 }
--- BaseMethodsAndPropertyDelegates/app/src/main/java/com/skillbox/homework6/Person.kt
+++ BaseMethodsAndPropertyDelegates/app/src/main/java/com/skillbox/homework6/Person.kt
@@ -1,10 +1,24 @@
 package com.skillbox.homework6
 
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 class Person (
     var height: Int,
     var weight: Int,
     var name: String
 ) {
+
+    val pets = hashSetOf<Animal>()
+
+    fun buyPet() {
+        val randomEnergy = Random.nextInt(1..100)
+        val randomWeight = Random.nextInt(1..100)
+        val randomName = ("Pet №" + Random.nextInt(1..100).toString())
+        val animal = Animal(randomEnergy,randomWeight,randomName)
+        pets.add(animal)
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -14,6 +28,7 @@ class Person (
         if (height != other.height) return false
         if (weight != other.weight) return false
         if (name != other.name) return false
+        if (pets != other.pets) return false
 
         return true
     }
@@ -22,6 +37,11 @@ class Person (
         var result = height
         result = 31 * result + weight
         result = 31 * result + name.hashCode()
+        result = 31 * result + pets.hashCode()
         return result
+    }
+
+    override fun toString(): String {
+        return "Person(height=$height, weight=$weight, name='$name', pets=$pets)"
     }
 }

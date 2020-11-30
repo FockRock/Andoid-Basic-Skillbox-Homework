d850c6db0b05a9e1e8ad9debfaaace94964f7258
FockRock <smirnov_nikita@mail.ru>
2020-11-30 22:48:39 +0300

    Homework 7 (1/12)

--- Extensions_Objects_Enums/app/src/main/java/com/skillbox/homework7/Ammo.kt
+++ Extensions_Objects_Enums/app/src/main/java/com/skillbox/homework7/Ammo.kt
@@ -1,11 +1,20 @@
 package com.skillbox.homework7
 
+import kotlin.random.Random
+
 enum class Ammo(
-    damage: Int,
-    criticalDamage: Int,
-    criticalHitRate: Int
+        private val damage: Int,
+        private val criticalHitRate: Int,
+        private val criticalDamage: Int
 ) {
-    PISTOL(1,10,5),
-    SHELLS(5,50,8),
-    EXPLOSIVE(10, 100, 10);
+    PISTOL(1,15,5),
+    SHELLS(5,23,7),
+    EXPLOSIVE(10, 40, 10);
+
+    fun takenDamage (damage: Int = this.damage,
+                     criticalHitRate: Int = this.criticalHitRate,
+                     criticalDamage: Int = this.criticalDamage): Int {
+        return if (criticalHitRate >= Random.nextInt(100)) damage*criticalDamage
+        else damage
+    }
 }
---
+++ Extensions_Objects_Enums/app/src/main/java/com/skillbox/homework7/Main.kt
@@ -0,0 +1,7 @@
+package com.skillbox.homework7
+
+fun main() {
+    println(Ammo.PISTOL.takenDamage())
+    println(Ammo.SHELLS.takenDamage())
+    println(Ammo.EXPLOSIVE.takenDamage())
+}

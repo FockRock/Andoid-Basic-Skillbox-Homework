15ccc1233aaf2acb67f1155e010d75560b3e3550
FockRock <smirnov_nikita@mail.ru>
2020-12-02 23:53:39 +0300

    Homework 7 (3/12) AbstractWeapon added

--- Extensions_Objects_Enums/app/src/main/java/com/skillbox/homework7/AbstractWeapon.kt
+++ Extensions_Objects_Enums/app/src/main/java/com/skillbox/homework7/AbstractWeapon.kt
@@ -1,3 +1,18 @@
 package com.skillbox.homework7
 
-abstract class AbstractWeapon(){}
+abstract class AbstractWeapon (
+        private val magazineSize: Int,
+        val fireType: FireType
+){
+    private val ammoList =  arrayOf<Ammo>()
+    val ammoAvailability: Boolean = ammoList.isNotEmpty()
+
+    abstract fun makeAmmo(): Ammo
+
+    fun reload() {
+        val ammoList = arrayOf<Ammo>()
+        for (each in ammoList) {
+            makeAmmo()
+        }
+    }
+}

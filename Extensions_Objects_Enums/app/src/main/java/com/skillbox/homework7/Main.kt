450a9b1742f3fdc45ca3d70b71fb3c3f120b9203
FockRock <smirnov_nikita@mail.ru>
2021-01-23 00:09:09 +0300

    Homework 8.4 started

--- Extensions_Objects_Enums/app/src/main/java/com/skillbox/homework7/Main.kt
+++ Extensions_Objects_Enums/app/src/main/java/com/skillbox/homework7/Main.kt
@@ -4,7 +4,7 @@ import kotlin.random.Random
 
 fun main() {
     println("Введите количество войнов в первой и второй команде команде:")
-    
+
     val war = Battle(Team(), Team(), false)
 
     war.battleIteration()

8959dedfa54c928c6a87d473ad7ad74765847bfb
FockRock <smirnov_nikita@mail.ru>
2021-01-25 22:08:34 +0300

    Homework 8.4 started

--- KotlinHighOrderFunctionAndException/01_Generics/app/src/main/java/com/skillbox/homework84/Main.kt
+++ KotlinHighOrderFunctionAndException/01_Generics/app/src/main/java/com/skillbox/homework84/Main.kt
@@ -2,8 +2,15 @@ package com.skillbox.homework84
 
 fun main() {
 
+    val list: List<Int> = listOf(1,2,4,5,6)
+    println(oddList(list))
 }
 
-fun oddList() {
-    
+class GenericList<T: Number>()
+
+fun oddList(list: GenericList<Number>): List<Number> {
+    var newList: List<Number>
+
+    for (t in list)
+        if (t % 2 == 0)
 }

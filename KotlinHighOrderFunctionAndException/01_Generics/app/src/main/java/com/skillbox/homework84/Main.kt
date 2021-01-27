13b2e2b73680749775736bfbde18c119e0abbe57
FockRock <smirnov_nikita@mail.ru>
2021-01-27 14:47:37 +0300

    Homework 8.4 started Part 1 done!

--- KotlinHighOrderFunctionAndException/01_Generics/app/src/main/java/com/skillbox/homework84/Main.kt
+++ KotlinHighOrderFunctionAndException/01_Generics/app/src/main/java/com/skillbox/homework84/Main.kt
@@ -2,18 +2,22 @@ package com.skillbox.homework84
 
 fun main() {
 
-    val newList: GenericClass<Double> =
-
-    println(oddList())
+    val newList = mutableListOf(4.36, 5, 7, 9, 6, 8, 5.4, 5.8, 4.63, 12.0, 136, 289, 37.4)
+    oddList(newList)
 }
 
-class GenericClass<out T: Number>() {
+fun <T: Number>oddList(newList: MutableList<T>): MutableList<T> {
+    val oddList: MutableList<T> = mutableListOf()
 
-}
+    for (t in newList) {
+        if (t.toDouble() % 2.0 == 0.0) {
+            oddList.add(t)
+        }
+    }
 
-fun oddList(newList: GenericClass<Number>): List<Int> {
-    val oddList: List<Int> = listOf()
-
+    println("Целые числа: ${newList.filter { it.toDouble() - it.toInt() == 0.0 }}")
+    println("Вещественные числа: ${newList.filter { it.toDouble() - it.toInt().toDouble() != 0.0 }}")
+    println("Четные числа: $oddList")
 
     return oddList
 }

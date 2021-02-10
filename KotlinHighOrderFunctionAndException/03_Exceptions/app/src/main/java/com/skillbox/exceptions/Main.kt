b6e77a6275896e9ba8a73a26609a558ee28ba62d
FockRock <smirnov_nikita@mail.ru>
2021-02-11 00:16:08 +0300

    Homework 8.8 completed!

--- KotlinHighOrderFunctionAndException/03_Exceptions/app/src/main/java/com/skillbox/exceptions/Main.kt
+++ KotlinHighOrderFunctionAndException/03_Exceptions/app/src/main/java/com/skillbox/exceptions/Main.kt
@@ -1,10 +1,39 @@
 package com.skillbox.exceptions
 
 fun main() {
-
+    val wheel = Wheel()
+    try {
+        wheel.setPressure(15.0)
+    } catch (e: Exception) {
+        println("Pressure is too HIGH")
+    }
+    try {
+        wheel.setPressure(-4.0)
+    } catch (e: Exception) {
+        println("Pressure is too LOW")
+    }
+    wheel.setPressure(3.0)
+    println("Pressure is OK")
 }
 
 class Wheel {
     var pressure: Double = 0.0
         private set
+
+    class TooHighPressure: Exception()
+    class TooLowPressure: Exception()
+    class IncorrectPressure: Exception()
+
+    fun setPressure(value: Double) {
+        pressure = value
+        if (value < 0) {
+            throw TooLowPressure()
+        } else if (value > 10) {
+            throw TooHighPressure()
+        }
+    }
+
+    fun check() {
+        if (pressure > 10 || pressure < 0) throw IncorrectPressure()
+    }
 } 

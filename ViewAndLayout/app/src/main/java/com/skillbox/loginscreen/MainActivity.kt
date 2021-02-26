c3f7641179b3ab08a2ed238ee943eb2ccb3df6b0
FockRock <smirnov_nikita@mail.ru>
2021-02-26 18:19:54 +0300

    Login screen created Homework 10 is complete!

--- ViewAndLayout/app/src/main/java/com/skillbox/loginscreen/MainActivity.kt
+++ ViewAndLayout/app/src/main/java/com/skillbox/loginscreen/MainActivity.kt
@@ -2,10 +2,68 @@ package com.skillbox.loginscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.Gravity
+import android.view.View
+import android.widget.CompoundButton
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Glide.with(this)
+            .load("https://248006.selcdn.ru/main/iblock/def/def82b733a444368cf333916fe4066bd/bb84628188b5382ecf36329e7410e5cc.png")
+            .into(image)
+
+        loginButton.isEnabled =
+            !(emailInput.text.toString() == "" && passwordInput.text.toString() == "" && !license.isChecked)
+
+        passwordInput.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                loginButton.isEnabled = s?.isNotEmpty() ?: false
+            }
+
+        })
+
+        loginButton.setOnClickListener {
+            val progressBar = ProgressBar(this).apply {
+                LinearLayout.LayoutParams(
+                    50,50
+                ).apply { gravity = Gravity.CENTER }
+                progressStart()
+            }
+            container.addView(progressBar)
+            progressStop(progressBar)
+        }
+    }
+
+    private fun progressStart() {
+        loginButton.isEnabled = false
+        emailInput.isEnabled = false
+        passwordInput.isEnabled = false
+        license.isEnabled = false
+    }
+
+    private fun progressStop(i: View) {
+        Handler().postDelayed({
+            i.visibility = View.GONE
+            loginButton.isEnabled = true
+            emailInput.isEnabled = true
+            passwordInput.isEnabled = true
+            license.isEnabled = true
+            Toast.makeText(this, R.string.success, Toast.LENGTH_SHORT).show()
+        }, 2000)
     }
 }

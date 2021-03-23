0b13e8fe44ece0c17486181b2fb155baaa6195e8
FockRock <smirnov_nikita@mail.ru>
2021-03-23 21:09:49 +0300

    Lesson 12 started 1/4 completed

--- ActivityLifecycle/app/src/main/java/com/skillbox/constraintlogin/MainActivity.kt
+++ ActivityLifecycle/app/src/main/java/com/skillbox/constraintlogin/MainActivity.kt
@@ -2,15 +2,21 @@ package com.skillbox.constraintlogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val tag = "Main Activity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(tag, "onCreate")
 
         Glide.with(this)
             .load("https://248006.selcdn.ru/main/iblock/def/def82b733a444368cf333916fe4066bd/bb84628188b5382ecf36329e7410e5cc.png")
@@ -21,5 +27,29 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this,"Enter it!",Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(tag, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(tag, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(tag, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(tag, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }

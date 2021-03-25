65440f06daeda6b73811d5fd17780f5b594cbf47
FockRock <smirnov_nikita@mail.ru>
2021-03-26 00:29:41 +0300

    Lesson 12 started 2/4 completed

--- ActivityLifecycle/app/src/main/java/com/skillbox/constraintlogin/MainActivity.kt
+++ ActivityLifecycle/app/src/main/java/com/skillbox/constraintlogin/MainActivity.kt
@@ -2,8 +2,14 @@ package com.skillbox.constraintlogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.Gravity
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
@@ -27,7 +33,63 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this,"Enter it!",Toast.LENGTH_SHORT).show()
             }
         }
+
+        anr.setOnClickListener {
+            Thread.sleep(15000)
+        }
+
+//        passwordInput.addTextChangedListener(textWatcher)
+//        emailInput.addTextChangedListener(textWatcher)
+//        license.setOnCheckedChangeListener { buttonView, isChecked ->
+//            loginButton.isEnabled = checking()
+//        }
+//
+//        loginButton.setOnClickListener {                                                            //После нажатия на кнопку создается прогресс-бар
+//            val progressBar = ProgressBar(this).apply {
+//                LinearLayout.LayoutParams(
+//                        50, 50
+//                ).apply { gravity = Gravity.CENTER }
+//                progressStart()
+//            }
+//            container.addView(progressBar)
+//            progressStop(progressBar)
+//        }
+
     }
+
+//    private val textWatcher = object : TextWatcher {                                                //Нужен для проверки заполненности текста
+//        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+//
+//        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+//
+//        override fun afterTextChanged(s: Editable?) {
+//            loginButton.isEnabled = checking()
+//        }
+//
+//    }
+//
+//    private fun progressStart() {                                                                   //Блокирует все кнопки на момент работы прогресс-бара
+//        loginButton.isEnabled = false
+//        emailInput.isEnabled = false
+//        passwordInput.isEnabled = false
+//        license.isEnabled = false
+//    }
+//
+//    private fun progressStop(i: View) {                                                             //Удаляет прогресс-бар и включает кнопки
+//        Handler().postDelayed({
+//            container.removeView(i)
+//            loginButton.isEnabled = true
+//            emailInput.isEnabled = true
+//            passwordInput.isEnabled = true
+//            license.isEnabled = true
+//            Toast.makeText(this, R.string.success, Toast.LENGTH_SHORT).show()
+//        }, 2000)
+//    }
+//
+//    private fun checking(): Boolean {                                                               //Метод проверки
+//        return emailInput.text.toString().isNotEmpty() &&
+//                passwordInput.text.toString().isNotEmpty() && license.isChecked
+//    }
 
     override fun onStart() {
         super.onStart()
